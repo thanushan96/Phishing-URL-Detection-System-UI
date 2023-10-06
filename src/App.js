d338c5cd1c70725685492b163608de11f057ba7f
thanushan96 <ravishankarthanushan96@gmail.com>
2023-10-06 09:08:31 +0530

    added Navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
-
+import Navbar from "./components/Navbar";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Navbar></Navbar>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,44 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-secondary ">
+      <a className="navbar-brand" href="#">
+        <i className="fas fa-rocket" style={{ marginLeft: "5px" }}></i> ML API
+      </a>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div
+        className="collapse navbar-collapse justify-content-end"
+        id="navbarNav"
+      >
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              <i className="fas fa-home"></i> Home
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              <i className="fas fa-info-circle"></i> About
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              <i className="fas fa-envelope"></i> Contact
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
